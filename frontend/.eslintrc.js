96c29e6616397c61b1e7ee1c575439be55619c02
Sai Karthik Ginni <sginni@pop-os.localdomain>
2021-09-14 18:00:30 -0400

    refactor(frontend): refactoring to add apollo

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  plugins: ['graphql'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'graphql/template-strings': [
+      'error',
+      {
+        // Import default settings for your GraphQL client. Supported values:
+        // 'apollo', 'relay', 'lokka', 'fraql', 'literal'
+        env: 'apollo',
+
+        // Import your schema JSON here
+        schemaJson: require('./schema.json'),
+
+        // OR provide absolute path to your schema JSON (but not if using `eslint --cache`!)
+        // schemaJsonFilepath: path.resolve(__dirname, './schema.json'),
+
+        // OR provide the schema in the Schema Language format
+        // schemaString: printSchema(schema),
+
+        // tagName is gql by default
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+}
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import VueCompositionAPI from '@vue/composition-api'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import { createProvider } from './vue-apollo'
 
-Vue.config.productionTip = false;
+Vue.use(VueCompositionAPI)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  apolloProvider: createProvider(),
+  render: h => h(App),
+}).$mount('#app')
