d21933db27c3dfe45b81ab06a8d59df8ea6bc15d
Sai Karthik Ginni <sginni@pop-os.localdomain>
2021-09-18 21:58:13 -0400

    feat(frontend): add persistedstate for rgbaster

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,12 +1,25 @@
 import { createStore, createLogger } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 import { RootState } from './types'
 
 import { store as pokedex, PokedexStore } from './modules/pokedex'
 
 export type Store = PokedexStore<Pick<RootState, 'pokedex'>>
 
+const isProd = process.env.NODE_ENV === 'production'
+
+const plugins = [
+  createPersistedState({
+    paths: ['pokedex.rgbaster'],
+  }),
+]
+
+if (!isProd) {
+  plugins.push(createLogger())
+}
+
 export const store = createStore<RootState>({
-  plugins: [createLogger()],
+  plugins,
   modules: {
     pokedex,
   },
